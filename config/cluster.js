bcb0ba30d7409fa538d9726aaafaef127f8b2538
michael.wei <dev.wei@gmail.com>
2014-11-07 00:15:53 -0500

    update

--- config/cluster.js
+++ config/cluster.js
@@ -1,99 +1,71 @@
 'use strict';
 
-var cluster = require('cluster'),
-    os = require('os'),
-    _ = require('underscore'),
-    string = require('string'),
-    logging = require('./logging'),
-    callback = require('../public/js/lib/my_components/callback/src/callback');
+var cluster = require('cluster');
+var os = require('os');
+var _ = require('lodash');
+var string = require('string');
+var logging = require('./logging');
 
 module.exports = function (options) {
 
-
-  // Constants
   var MASTER = 'MASTER';
-  var WORKER = 'WORKER_{{index}}';
+  var WORKER = _.template('WORKER_<%=index%>');
 
-  // WorkerListeners, this is used for storing all callbacks for each worker
-  var workerListeners = new callback();
-
-  /**
-   * Default options
-   * @type {{workerCount: *, workerListeners: Array}}
-   */
   var defaultsOptions = {
-    workerCount: os.cpus().length,
-    workerListeners: []
+    workerCount: os.cpus().length
   };
 
-  /**
-   * Check options
-   */
   options = _.extend(defaultsOptions, options);
   (function (options) {
     if (typeof options.workerCount !== 'number') {
       throw 'workerCount be a number';
     }
-    options.workerListeners.forEach(function (listener) {
-      if (typeof listener !== 'function') {
-        throw 'workerListeners must only contain function(s).';
-      }
-      workerListeners.add(listener);
-    });
   }(options));
 
-  /**
-   * Master function
-   */
   var runMaster = function () {
     var logger = logging.getLogger(MASTER);
-    logger.debug('starting master......');
+    logger.debug('Starting MASTER......');
 
     var workers = [];
-    var i;
-    for (i = 0; i < options.workerCount; i = i + 1) {
+    for (var i = 0; i < options.workerCount; i = i + 1) {
       workers.push(cluster.fork());
     }
 
     cluster.on('exit', function (worker, code) {
       var workerIdx = workers.indexOf(worker);
-      var logger = logging.getLogger(string(WORKER).template({"index": cluster.worker.id}).s);
+      var logger = logging.getLogger(
+        WORKER({"index": cluster.worker.id}));
 
       logger.warning(worker.process.pid + ' died with code ' + code);
-      logger.info('restarting.....');
+      logger.info('Restarting.....');
 
       if (workerIdx > -1) {
         workers.splice(workerIdx, 1);
       }
 
       worker = cluster.fork();
-      worker.on('message', function () {
-        workerListeners.handle(true);
-      });
-
       return workers.push(worker);
     });
 
-    logger.debug('ending master......');
-    return process.on('SIGQUIT', function () {
-      var j, worker, results;
-      results = [];
+    logger.debug('Ending MASTER......');
+    process.on('SIGQUIT', function () {
+      var results = [];
 
       logger.info('QUIT received, will exit once all workers have finished current requests');
-      for (j = 0; j < workers.length; j += 1) {
-        worker = workers[j];
+      for (var j = 0; j < workers.length; j += 1) {
+        var worker = workers[j];
         results.push(worker.send('quit'));
       }
       return results;
     });
+
+    return logger;
   };
 
-  /**
-   *
-   */
   var runWorker = function () {
-    var logger = logging.getLogger(string(WORKER).template({"index": cluster.worker.id}).s);
-    logger.debug('starting worker......');
+    var logger = logging.getLogger(
+      WORKER({"index": cluster.worker.id}));
+    logger.debug('Starting Worker......');
 
 //        server = loader(workerLogger);
 //        if (!server) {
@@ -114,7 +86,8 @@ module.exports = function (options) {
 //            });
 //        }
 
-    logger.debug('ending worker......');
+    logger.debug('Ending Worker......');
+    return logger;
   };
 
   if (cluster.isMaster) {
