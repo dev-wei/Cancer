05b4249e99b92dcd76dc027f3d15dcfb5e168493
michael.wei <dev.wei@gmail.com>
2014-11-19 21:33:17 -0500

    changes

--- public/js/modules/app/app.js
+++ public/js/modules/app/app.js
@@ -6,14 +6,12 @@ var ngModule = angular.module('cancer.app',
   [
     'ui.router',
     'ngResource',
-    'ngSanitize'
-  ]);
-  //.config(function ($httpProvider, $locationProvider) {
-  //  $httpProvider.defaults.useXDomain = true;
-  //  $locationProvider.html5Mode(true);
-  //})
-  //.run(function ($animate) {
-  //  $animate.enabled(false);
-  //});
+    'ngSanitize',
+    'ui.bootstrap'
+  ])
+  .config(function ($httpProvider, $locationProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    $locationProvider.html5Mode(true);
+  });
 
 module.export = ngModule;
